4fa3a74750e95cc93252e5fb3560dad874c7342d
QuantumMadi <45874448+QuantumMadi@users.noreply.github.com>
2019-05-09 23:37:06 +0600

    nudity service api checking

---
+++ DeepAi.Service/DeepAi.Service/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeepAI;
+
+namespace DeepAi.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeepAI_API api = new DeepAI_API(apiKey: "c374d52f-84ce-46a6-8a4f-598ba5b9e988");
+            StandardApiResponse resp = api.callStandardApi("nsfw-detector", new
+            {
+                image = File.OpenRead(@"C:\Users\www\Desktop\1\sasha2.jpg"),
+            });
+            Console.WriteLine(api.objectAsJsonString(resp));
+            Console.ReadLine();
+        }
+    }
+}
